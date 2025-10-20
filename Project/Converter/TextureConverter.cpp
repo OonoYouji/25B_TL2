c998fe7d23646c743505f0746e1abe8676c67eb2
OonoYouji <k023g0007@g.neec.ac.jp>
2025-10-20 13:18:25 +0900

    dds変換するときに圧縮してサイズを小さくした

--- Project/Converter/TextureConverter.cpp
+++ Project/Converter/TextureConverter.cpp
@@ -80,12 +80,45 @@ std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::str
 }
 
 void TextureConverter::SaveDDSTextureToFile() {
+
+	/// MipMapを生成
+	DirectX::ScratchImage mipChain;
+	HRESULT hr = DirectX::GenerateMipMaps(
+		scratchImage_.GetImages(),
+		scratchImage_.GetImageCount(),
+		scratchImage_.GetMetadata(),
+		DirectX::TEX_FILTER_DEFAULT,
+		0, mipChain
+	);
+
+	if (SUCCEEDED(hr)) {
+		scratchImage_ = std::move(mipChain);
+		metadata_ = scratchImage_.GetMetadata();
+	}
+
+	/// 圧縮形式に変換
+	DirectX::ScratchImage converted;
+	hr = DirectX::Compress(
+		scratchImage_.GetImages(),
+		scratchImage_.GetImageCount(),
+		metadata_,
+		DXGI_FORMAT_BC7_UNORM_SRGB,
+		DirectX::TEX_COMPRESS_BC7_QUICK | DirectX::TEX_COMPRESS_SRGB_OUT | DirectX::TEX_COMPRESS_PARALLEL,
+		1.0f, converted
+	);
+
+	if (SUCCEEDED(hr)) {
+		scratchImage_ = std::move(converted);
+		metadata_ = scratchImage_.GetMetadata();
+	}
+
+
 	/// SRGBとして扱う
 	metadata_.format = DirectX::MakeSRGB(metadata_.format);
 
 	/// 出力ファイルを設定、DDSファイルに書き出す
 	std::wstring filepath = directoryPath_ + fileName_ + L".dds";
-	HRESULT hr = DirectX::SaveToDDSFile(
+	hr = DirectX::SaveToDDSFile(
 		scratchImage_.GetImages(),
 		scratchImage_.GetImageCount(),
 		metadata_,
